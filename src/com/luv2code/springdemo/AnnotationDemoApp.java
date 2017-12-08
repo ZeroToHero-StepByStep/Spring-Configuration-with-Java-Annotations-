dadc1885684b06bb1bed44d3b1870e01122bc7d1
Jack <jzha471@aucklanduni.ac.nz>
2017-12-09 12:25:52 +1300

    56. Default Component Names - Write Some Code

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -11,7 +11,7 @@ public class AnnotationDemoApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml") ; 
 		
 		// get the bean spring container
-		Coach coach = context.getBean("thatSillyCoach", Coach.class) ;
+		Coach coach = context.getBean("tennisCoach", Coach.class) ;
 		
 		// call a method on the bean
 		System.out.println(coach.getDailyWorkout()) ;
