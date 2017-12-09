67a1ce7ad83d09eefc476d635f3f4f83f66160fb
Jack <jzha471@aucklanduni.ac.nz>
2017-12-09 13:29:26 +1300

    71. Qualifiers for Dependency Injection - Write Some Code - Part 2

--- src/com/luv2code/springdemo/RandomFortuneService.java
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,14 +1,32 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	// create an array of strings
+	private String[] data = {
+			"The cut trolley persists beside ",
+			"The coincidence hangs a ",
+			"Next to the dip resides the pending "
+	};
+	
+	
+	// create a random number generator 
+	private Random myRandom = new Random() ;
+	
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// pick	a random string from the array 
+		int index = myRandom.nextInt(data.length) ;
+		String fortune = data[index] ;
+		return fortune;
 	}
+	
+	
 
 }
