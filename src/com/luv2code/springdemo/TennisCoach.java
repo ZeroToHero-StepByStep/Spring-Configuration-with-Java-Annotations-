9889a519616aa34c552796e6951f3f954fdfdbbd
Jack <jzha471@aucklanduni.ac.nz>
2017-12-09 13:03:19 +1300

    67. Field Injection - Write Some Code

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService ;
 	
 //	@Autowired
@@ -31,11 +32,11 @@ public class TennisCoach implements Coach {
 	}
 	
 	
-	@Autowired
-	public void doSomeCrazyStuff( FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-		this.fortuneService = fortuneService ;
-	}
+//	@Autowired
+//	public void doSomeCrazyStuff( FortuneService fortuneService) {
+//		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//		this.fortuneService = fortuneService ;
+//	}
 	
 	
 }
