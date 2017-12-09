33245112d367e8020969e6453dacb8e8dab6c8c8
Jack <jzha471@aucklanduni.ac.nz>
2017-12-10 11:19:22 +1300

    85. Defining Spring Beans with Java Code - Write Some Code - Part 1

---
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+public class SwimCoach implements Coach {
+
+	
+	private FortuneService fortuneService ;
+	
+	public SwimCoach( FortuneService fortuneService) {
+		this.fortuneService = fortuneService ;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Swim 1000 meters as a warm up.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return this.fortuneService.getFortune();
+	}
+
+}
